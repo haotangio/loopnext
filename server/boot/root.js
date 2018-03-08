583530db6cdc4de15960e375f4087122fb488b82
Hao Tang <haotc@dgroup.co>
2018-03-08 09:53:38 +0700

    Init project

---
+++ server/boot/root.js
@@ -0,0 +1,6 @@
+module.exports = function (server) {
+  // Install a `/` route that returns server status
+  const router = server.loopback.Router()
+  router.get('/', server.loopback.status())
+  server.use(router)
+}
