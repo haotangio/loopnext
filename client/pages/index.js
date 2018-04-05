70a99b86b69267b13659ea57851d3c66eb37e885
Hao Tang <haotc@dgroup.co>
2018-04-05 16:34:33 +0700

    Add sass support

---
+++ client/pages/index.js
@@ -0,0 +1,3 @@
+import '../scss/style.scss'
+
+export default () => <div className='example'>Hello World!</div>
--- server/boot/root.js
+++ server/boot/root.js
@@ -1,6 +1,24 @@
-module.exports = function (server) {
-  // Install a `/` route that returns server status
+const path = require('path')
+const next = require('next')
+const routes = require('../../common/routes')
+
+const dev = process.env.NODE_ENV !== 'production'
+
+module.exports = async function (server) {
   const router = server.loopback.Router()
-  router.get('/', server.loopback.status())
+
+  const nextInstance = next({
+    dev,
+    dir: path.join(__dirname, '../../client')
+  })
+  await nextInstance.prepare()
+  const handle = routes.getRequestHandler(nextInstance)
+  router.get('*', (req, res, next) => {
+    if (req.url.indexOf('/api') === 0) {
+      return next()
+    }
+    handle(req, res, next)
+  })
+
   server.use(router)
 }
