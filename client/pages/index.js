f0a3b973cab9bca490702a497a6677587424faee
Hao Tang <haotc@dgroup.co>
2018-04-05 17:21:26 +0700

    Header for webpage

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,28 @@
+import Link from 'next/link'
+// import Router from 'next/router'
+import Head from 'next/head'
 import '../scss/style.scss'
 
-export default () => <div className='example'>Hello World!</div>
+export default () =>
+  <div>
+    <Head>
+      <title>Welcome to loopnext</title>
+    </Head>
+    <div className='app-navbar'>
+      <div className='logo'>LoopNext</div>
+      <div className='links'>
+        <Link href='/login'>
+          <a className='link'>Login</a>
+        </Link>
+        <Link href='/login'>
+          <a className='link'>Signup</a>
+        </Link>
+      </div>
+    </div>
+    <div className='app-body'>
+      <div>App body</div>
+    </div>
+    <div className='app-footer'>
+      <div>App footer</div>
+    </div>
+  </div>
--- common/routes.js
+++ common/routes.js
@@ -2,7 +2,5 @@ const routes = require('next-routes')()
 
 routes
   .add('login')
-  .add('dashboard')
-  .add('/cvs/:id/edit', 'cv-edit')
 
 module.exports = routes
